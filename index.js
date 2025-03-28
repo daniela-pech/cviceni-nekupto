9200afec025ad3f93f8e700a707a08f6c2f67c95
Daniela Pech <randakova.daniela@gmail.com>
2025-03-28 16:26:24 +0100

    prní část ukolu

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const p1 = document.querySelector('#product1');
+p1.classList.add('border-primary');
+
+const p2 = document.querySelector('#product2 button');
+p2.classList.remove('btn-primary');
+
+const p3 = document.querySelector('#product3 .card-title');
+p3.classList.toggle('text-center'); //na střed
+p3.classList.toggle('text-center'); //na střed se zruší
+p3.classList.toggle('text-center'); // na střed se opět zavolá
