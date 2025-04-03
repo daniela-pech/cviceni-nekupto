f704f6ec0ecabb1f1c3317f25bba0c7b8da7f585
Daniela Pech <randakova.daniela@gmail.com>
2025-04-03 16:06:36 +0200

    vypracovane obe casti cviceni, queryselector, innerHTML

--- index.js
+++ index.js
@@ -19,13 +19,53 @@ const product3 = {
     'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
+// PRVNÍ ČÁST
 const p1 = document.querySelector('#product1');
 p1.classList.add('border-primary');
-
+/*
 const p2 = document.querySelector('#product2 button');
 p2.classList.remove('btn-primary');
 
 const p3 = document.querySelector('#product3 .card-title');
 p3.classList.toggle('text-center'); //na střed
-p3.classList.toggle('text-center'); //na střed se zruší
-p3.classList.toggle('text-center'); // na střed se opět zavolá
+*/
+//DRUHÁ ČÁST
+
+const product1HTML = `
+<img class="card-img-top" src=${product1.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`;
+
+const p1Element = document.querySelector('#product1');
+p1Element.innerHTML = product1HTML;
+
+const product2HTML = `
+<img
+            class="card-img-top"
+            src=${product2.image}
+            alt="Slepičkabelka"
+          />
+          <div class="card-body">
+            <h2 class="card-title">${product2.name}</h2>
+            <p class="card-text">
+             ${product2.description}
+            </p>
+          </div>
+          <button type="button" class="btn btn-lg btn-primary btn-block">
+            Koupit</button>`;
+const p2Element = document.querySelector('#product2');
+p2Element.innerHTML = product2HTML;
+
+const product3HTML = `
+ <img class="card-img-top" src=${product3.image} alt="Anatoplavky">
+          <div class="card-body">
+            <h2 class="card-title">${product3.name}</h2>
+            <p class="card-text">${product3.description}</p>
+          </div>
+          <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const p3Element = document.querySelector('#product3');
+p3Element.innerHTML = product3HTML;
